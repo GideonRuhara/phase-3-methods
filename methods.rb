00d55c39847768406f67ea8243be7744e34e03f2
Gideon Ruhara <ruharagideon@gmail.com>
2023-02-13 21:21:26 +0300

    functions completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
 # Your code here!
+#first test 
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+#second test 
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+#third test 
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+#fourth test 
+def add(num1, num2)
+    return num1 + num2
+end
+
+#fifth test 
+def halve(num)
+    return nil unless num.class == Integer
+    num / 2
+end
